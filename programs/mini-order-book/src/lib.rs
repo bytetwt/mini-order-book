7afc7801ecba0a19472b8361fba64785a1be2865
bytetwt <bytehash69@gmail.com>
2026-05-12 12:08:26 +0530

    feat: add account states

--- programs/mini-order-book/src/lib.rs
+++ programs/mini-order-book/src/lib.rs
@@ -2,6 +2,8 @@ use anchor_lang::prelude::*;
 
 declare_id!("HhYnYsWtT6nCWL3Mw4dZgoicScQd8VcwHUJfk8fdv8Lq");
 
+pub mod state;
+
 #[program]
 pub mod mini_order_book {
     use super::*;
---
+++ programs/mini-order-book/src/state/mod.rs
@@ -0,0 +1,43 @@
+use anchor_lang::prelude::{
+    borsh::{BorshDeserialize, BorshSerialize},
+    *,
+};
+
+#[account]
+pub struct Market {
+    pub authority: Pubkey,
+    pub ask_count: u64,
+    pub bid_count: u64,
+    pub bump: u8,
+}
+
+#[account]
+pub struct Ask {
+    pub market: Pubkey,
+    pub seller: Pubkey,
+    pub price: u64,
+    pub original_size: u64,
+    pub remaining_size: u64,
+    pub status: OrderStatus,
+    pub created_at: i64,
+    pub bump: u8,
+}
+
+pub struct Bid {
+    pub market: Pubkey,
+    pub buyer: Pubkey,
+    pub price: u64,
+    pub original_size: u64,
+    pub remaining_size: u64,
+    pub status: OrderStatus,
+    pub created_at: i64,
+    pub bump: u8,
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Clone)]
+pub enum OrderStatus {
+    Open,
+    PartiallyFilled,
+    Filled,
+    Cancelled,
+}
